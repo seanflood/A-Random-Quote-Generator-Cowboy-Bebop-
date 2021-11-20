f3d0c599635f800fafaaf06d600c97c2282cf4a7
SEAN FLOOD <42306842+seanflood@users.noreply.github.com>
2021-11-19 21:03:12 -0500

    Update script.js

--- Quote Generator/js/script.js
+++ Quote Generator/js/script.js
@@ -3,16 +3,17 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+/**
+ * A Random Quote Generator using quotes from the tv show "Cowyboy Bebop"
+ * Sean Flood
+ * Nov 19, 2021
+ * 
+ */
 
 
-
-
-/*** 
- * `quotes` array 
-***/
+/**
+* An array of 'quotes' objects
+*/
 const quotes = [
   {
     quote: "They say hunger is the best spice.",
@@ -26,44 +27,35 @@ const quotes = [
     source: "Spike Spiegel", 
     citation: "Wild Horses", 
     timeStamp: '22:00',
-    
-
   }, 
   {
-    quote:  "I'm not going there to die. I'm going to find out if I'm really alive."
-    ,
+    quote:  "I'm not going there to die. I'm going to find out if I'm really alive.",
     source: "Spike Spiegel", 
     citation: "The Real Folk Blues, Part II", 
     timeStamp: '10:25',
     year: 1997
   }, 
   {
-    quote: "Don't you want to hang out and waste your life with us?"
-    ,
+    quote: "Don't you want to hang out and waste your life with us?",
     source: "Spike Spiegel", 
     citation: "Knockin On Heaven's Door", 
     timeStamp: '10:25',
-    
   }, 
   {
-    quote: "Angels banished from heaven have no choice but to become devils."
-    ,
+    quote: "Angels banished from heaven have no choice but to become devils.",
     source: "Vicious", 
     citation: "Ballad Of Fallen Angels ", 
     timeStamp: '15:20',
     year: 1997
   }, 
   {
-    quote: "When we show fear, it jumps at us faster than light. But, if we do not show fear, it casts its eye upon us gently and then guides us into infinity."
-    ,
+    quote: "When we show fear, it jumps at us faster than light. But, if we do not show fear, it casts its eye upon us gently and then guides us into infinity.",
     source: "Laughing Bull", 
     citation: "The Real Folk Blues, Part II", 
     timeStamp: '10:25',
-    
   }, 
   {
-    quote: "You know the first rule of combat? Shoot them before they shoot you." 
-    ,
+    quote: "You know the first rule of combat? Shoot them before they shoot you.",
     source: "Faye Valentine", 
     citation: "Honky Tonk Women", 
     timeStamp: '2:30',
@@ -74,11 +66,9 @@ const quotes = [
     source: "Jet Black", 
     citation: "The Real Folk Blues, Part II", 
     timeStamp: '10:25',
-    
   }, 
   {
-    quote: "Why don’t we drink to me and my reflection in your lovely eyes?"
-    ,
+    quote: "Why don’t we drink to me and my reflection in your lovely eyes?",
     source: "Andy Von De Oniyate", 
     citation: "The Real Folk Blues, Part II", 
     timeStamp: '15:23',
@@ -88,8 +78,7 @@ const quotes = [
     quote: "Where are you? Are you here Mr. spooky space creature?",
     source: "Radical Edward", 
     citation: "Toys In The Attic", 
-    timeStamp: '18:15',
-    
+    timeStamp: '18:15',    
   }, 
   {
     quote: "High socks are coo-ool. Gotta wear 'em outside!", 
@@ -103,7 +92,6 @@ const quotes = [
     source: "Radical Edward", 
     citation: "Brain Scratch", 
     timeStamp: '12:32', 
-   
   }, 
   {
     quote: "Woof",
@@ -115,16 +103,24 @@ const quotes = [
 ];
 
 
-
-/***
- * `getRandomQuote` function
-***/  
+/**
+ * `getRandomQuote` function. Using Math.random and Math.floor, returns a random object
+ *  from 'quotes' array.
+*/  
 function getRandomQuote(){
   for (let i = 0; i < quotes.length; i++){
       let thing = Math.floor(Math.random() * quotes.length) + i;
       return quotes[thing]; 
   }
 }; 
+
+
+/**
+*  -changeBackColor() function declares a variable for each of the 3 rgb values and
+*  sets their value to a ranom number between 1-256
+*
+* -backColor variable is set to an interpolated string with 3 new rgb values and then updates DOM with new values
+*/
 
 
 
@@ -140,9 +136,11 @@ function changeBackColor(){
 
 
 
-/***
- * `printQuote` function
-***/
+/**
+ * `printQuote` function declares 'markUp'variable and uses interpolation
+ * to update DOM with 'quotes' array object values (quote, source, and timestamp)
+ * The values 'year' and 'citation' are only displayed if applicable.
+*/
 
 function printQuote(){
   changeBackColor();
@@ -162,23 +160,18 @@ function printQuote(){
 
 }; 
 
+
+/**
+* changeQuote() function uses setInterval() method to change displayed quote
+* every 8 seconds calling the printQuote() function.
+*/
+
 function changeQuote(){
-  return setInterval(printQuote,5000); 
+  return setInterval(printQuote,8000); 
 };
 
 
 changeQuote();
- 
-
-
-
-
-
-//Create function that changes background color 
-
-
-
-
 
 
 /***
@@ -188,13 +181,3 @@ changeQuote();
 
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
-
-
-
-
-
-
-
-//Take out some here and there parts for project rules
-//Write Comments
-//Try adding pictures
